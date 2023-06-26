5dec269e793f02c2b2b691832a119d6f7d5d7062
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-06-26 16:31:20 -0300

    feat: inserindo campos opcionais inproceedings

--- src/main/java/org/jabref/model/entry/types/BibtexEntryTypeDefinitions.java
+++ src/main/java/org/jabref/model/entry/types/BibtexEntryTypeDefinitions.java
@@ -22,7 +22,7 @@ public class BibtexEntryTypeDefinitions {
     private static final BibEntryType ARTICLE = new BibEntryTypeBuilder()
             .withType(StandardEntryType.Article)
             .withRequiredFields(StandardField.AUTHOR, StandardField.TITLE, StandardField.JOURNAL, StandardField.YEAR)
-            .withImportantFields(StandardField.VOLUME, StandardField.NUMBER, StandardField.PAGES, StandardField.MONTH, StandardField.ISSN, StandardField.NOTE, StandardField.ADDRESS, StandardField.KEYWORDS, StandardField.ABSTRACT, StandardField.URL, StandardField.DOI)
+            .withImportantFields(StandardField.VOLUME, StandardField.NUMBER, StandardField.PAGES, StandardField.MONTH, StandardField.ISSN, StandardField.NOTE, StandardField.ADDRESS, StandardField.KEYWORDS, StandardField.ABSTRACT, StandardField.URL, StandardField.DOI, StandardField.ANNOTE)
             .build();
 
     /**
@@ -93,7 +93,7 @@ public class BibtexEntryTypeDefinitions {
     private static final BibEntryType INPROCEEDINGS = new BibEntryTypeBuilder()
             .withType(StandardEntryType.InProceedings)
             .withRequiredFields(StandardField.AUTHOR, StandardField.TITLE, StandardField.BOOKTITLE, StandardField.YEAR)
-            .withImportantFields(StandardField.EDITOR, StandardField.VOLUME, StandardField.NUMBER, StandardField.SERIES, StandardField.PAGES, StandardField.ADDRESS, StandardField.MONTH, StandardField.ORGANIZATION, StandardField.PUBLISHER, StandardField.NOTE)
+            .withImportantFields(StandardField.EDITOR, StandardField.VOLUME, StandardField.NUMBER, StandardField.SERIES, StandardField.PAGES, StandardField.ADDRESS, StandardField.MONTH, StandardField.ORGANIZATION, StandardField.PUBLISHER, StandardField.NOTE, StandardField.ADDRESS, StandardField.ANNOTE)
             .build();
 
     /**
